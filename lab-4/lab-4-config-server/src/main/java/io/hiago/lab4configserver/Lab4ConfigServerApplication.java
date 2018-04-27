0f31b2dc559ea437532b17d2616866928bf6bc45
Hiago Oliveira <hiago.oliveira@preventsenior.com.br>
2018-04-27 12:06:25 -0300

    Lab 4 - Get Eureka url from Config server

---
+++ lab-4/lab-4-config-server/src/main/java/io/hiago/lab4configserver/Lab4ConfigServerApplication.java
@@ -0,0 +1,14 @@
+package io.hiago.lab4configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class Lab4ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab4ConfigServerApplication.class, args);
+    }
+}
